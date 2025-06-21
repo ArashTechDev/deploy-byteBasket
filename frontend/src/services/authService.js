a166c7a6f4986503eeb221b9c0110a4ae8ac5571
Ulas Ondev <ulascondev@gmail.com>
2025-06-20 23:43:36 -0400

    Create authService.js

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const API = '/api/auth';  
+
+export const registerUser = async (userData) => {
+  const res = await axios.post(`${API}/register`, userData);
+  return res.data;
+};
+
+export const loginUser = async (credentials) => {
+  const res = await axios.post(`${API}/login`, credentials);
+  return res.data;
+};
+
+export const logoutUser = async () => {
+  const res = await axios.post(`${API}/logout`);
+  return res.data;
+};
+
+export const verifyEmail = async (token) => {
+  const res = await axios.get(`${API}/verify-email?token=${token}`);
+  return res.data;
+};
+
+export const getCurrentUser = async () => {
+  const res = await axios.get(`${API}/me`);
+  return res.data;
+};
+
+export const getDashboard = async () => {
+  const res = await axios.get(`${API}/dashboard`);
+  return res.data;
+};
