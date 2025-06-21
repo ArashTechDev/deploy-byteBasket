caf53167719f120f287bcbbbf2854b0386914605
Ulas Ondev <ulascondev@gmail.com>
2025-06-21 11:45:06 -0400

    Update User.js

--- backend/src/db/models/users/User.js
+++ backend/src/db/models/users/User.js
@@ -90,4 +90,4 @@ userSchema.methods.correctPassword = async function(candidatePassword, userPassw
   return await bcrypt.compare(candidatePassword, userPassword);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
