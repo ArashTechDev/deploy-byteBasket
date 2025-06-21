67f59c1dea5546c137c205ba394bbb39a8aa3f6d
Ulas Ondev <ulascondev@gmail.com>
2025-06-21 11:49:09 -0400

    Update mongodb.js

--- backend/src/config/mongodb.js
+++ backend/src/config/mongodb.js
@@ -6,7 +6,7 @@ const connectMongoDB = async () => {
     const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/bytebasket';
     
     const options = {
-      dbName: process.env.MONGO_DB_NAME || 'ByteBasket',
+      dbName: process.env.MONGO_DB_NAME || 'bytebasket',
       maxPoolSize: 10, // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
